36454734a8f367af9ab23430bf50988ff003dd92
unknown <abenavid@win.redhat.com>
2023-05-29 20:24:22 -0500

    update. move everyhting to python script

--- README.md
+++ README.md
@@ -1,23 +1,10 @@
 # data-automation
 1. web-scrape.py
  > * This is optional if you need to scrape the web to ingest .csv files into your local server
-2. init-py-dataclean.py
+2. data-mover.py
  > *  Used to modify .csv files into appropriate format (i.e. removing unwanted rows/columns, changing date to SQL readable format, etc.)
  > *  Creates copy of .csv file in a temp folder where ETL pipeline will grab from and set data into SQL DB
-3. SSIS project to move .csv file from tmp folder into SQL Server
-4. Create Windows Taks Scheduler for init-pyh-dataclean.py
+ > *  Inserts data into SQL DB
+3. Create Windows Taks Scheduler for data-mover.py
 >  * you can use this to repeat however frequently you need
-5. Create ETL Pipeline
->  * similar to step 4, but this triggers the SSIS script (within SSMS)
-6. Display your data from SQL Database via PowerBI
-
-## SSIS Project Setup
-* Download Integration Services extension on Visual Studio
-* Create a project using the 'Integration Services Project Business Intelligence' template
-* Create a connection to your SQL DB using ADO.NET type
-* Create 2 variables
- > * Filepath, type string, directory path to your tmp folder
- > * TableName, type string, the name of your table in the db you've specified
-* Drag 'Script Task' to the middle box in order to create a script
-* Link the 2 variables you created earlier to the script
-* Use the code 'SQLtoDB' for your script
+4. Display your data from SQL Database via PowerBI
--- python-scripts/data-mover.py
+++ python-scripts/data-mover.py
@@ -2,24 +2,73 @@ import datetime
 import shutil
 import os
 import pandas as pd
+from sqlalchemy import create_engine, inspect
+import psycopg2
+import pyodbc
+import re
 
-# Note - use '/' instead of '\' on paths
-tmp_dir_path = ""  # TODO update tmp dir path to your folder which will feed data to SSIS project
-data_dir_path = ""  # TODO update data dir path to the folder where your .csv files exist
+# Variables
+Server = ''
+Database = ''
+username = ""
+password = ''
+Driver = 'ODBC Driver 17 for SQL Server'
+table_name = ''
+pk_column_name = ''
+tmp_dir_path = ""  
+data_dir_path = ""  
 
-old_file = os.listdir(tmp_dir_path)
+### Data cleaning 
 
+# Grab files
 data_files = [f for f in os.listdir(data_dir_path) if '.csv' in f.lower()]
-new_file = data_files[-1] # use 0 for first, use -1 for last (the purpose of this is to grab the .csv files in order)
+new_file = data_files[-1] # use 0 for first, use -1 for last
 
-if old_file[0] != new_file:
-  shutil.rmtree(tmp_dir_path)
-  os.mkdir(tmp_dir_path)
-  df = pd.read_csv(data_dir_path + '/' + new_file)
-  df['Time']=pd.to_datetime(df['Time']).apply(lambda s:s.isoformat())
-  df.to_csv(data_dir_path + '/' + new_file, index=False)
-  dest = tmp_dir_path + '/' + new_file
-  shutil.move(data_dir_path + '/' + new_file, dest)
-  print("SUCCESS: File moved into tmp folder")
+# Clean dir and move file
+shutil.rmtree(tmp_dir_path)
+os.mkdir(tmp_dir_path)
+dest = tmp_dir_path + '/' + new_file
+shutil.copy(data_dir_path + '/' + new_file, dest)
+df = pd.read_csv(tmp_dir_path + '/' + new_file)
+
+# Data manipulation
+df[pk_column_name] = pd.to_datetime(df[pk_column_name], dayfirst=True).apply(lambda s: s.isoformat())
+df.to_csv(tmp_dir_path + '/' + new_file, index=False)
+
+### SQL logic
+
+# Database connection
+Database_con = f'mssql://@{Server}/{Database}?driver={Driver}'
+engine = create_engine(Database_con)
+con = engine.connect()
+
+# Read existing data from the database
+df = pd.read_sql_query(f"Select * from [stirling_db].[dbo].[{table_name}]", con)
+
+# Read new data from the CSV file
+dp = pd.read_csv(tmp_dir_path + '/' + new_file)
+dp.columns = [re.sub(r'[-\s/]+', '_', col.strip()).strip('_').replace('(', '').replace(')', '') for col in dp.columns]
+dp.columns = [re.sub(r'[^a-zA-Z0-9_]', '', col) for col in dp.columns]
+
+# Check if table exists
+inspector = inspect(engine)
+table_exists = inspector.has_table(table_name)
+
+if table_exists:
+  # Check for duplicates
+  existing_times_query = f"SELECT {pk_column_name} FROM {table_name}"
+  existing_times = pd.read_sql(existing_times_query, engine)
+  
+  # Convert the primary key column values to match the database format
+  dp[pk_column_name] = pd.to_datetime(dp[pk_column_name]).dt.strftime('%Y-%m-%d %H:%M:%S')
+  
+  # Filter out duplicates
+  dp_filtered = dp[~dp[pk_column_name].astype(str).isin(existing_times[pk_column_name].astype(str))]
 else:
-  print("ERR: SKIPPING, duplicate file detected" + new_file)
+  dp_filtered = dp
+
+# Ensure none of the column names end with an underscore
+dp_filtered.columns = [col.rstrip('_') if col.endswith('_') else col for col in dp_filtered.columns]
+
+# Write the DataFrame to the SQL table
+dp_filtered.to_sql(table_name, engine, if_exists='append', index=False)
